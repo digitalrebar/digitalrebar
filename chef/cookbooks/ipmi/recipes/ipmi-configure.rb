5d61cf4eacbd780ad6599ba70cc5e7744a359ba6
Greg Althaus <galthaus@austin.rr.com>
2011-10-19 20:39:17 -0500

    DE429: Make the ipmi code run once if successful.

--- chef/cookbooks/ipmi/recipes/ipmi-configure.rb
+++ chef/cookbooks/ipmi/recipes/ipmi-configure.rb
@@ -32,16 +32,18 @@ end
 
 bmc_user     = node[:ipmi][:bmc_user]
 bmc_password = node[:ipmi][:bmc_password]
-bmc_address  = node[:crowbar][:network][:bmc][:address]
-bmc_netmask  = node[:crowbar][:network][:bmc][:netmask]
-bmc_router   = node[:crowbar][:network][:bmc][:router]
+bmc_address  = node["crowbar"]["network"]["bmc"]["address"]
+bmc_netmask  = node["crowbar"]["network"]["bmc"]["netmask"]
+bmc_router   = node["crowbar"]["network"]["bmc"]["router"]
 
 node["crowbar"] = {} if node["crowbar"].nil?
 node["crowbar"]["status"] = {} if node["crowbar"]["status"].nil?
-node["crowbar"]["status"]["ipmi"] = {} if node["crowbar"]["status"]["ipmi"].nil?
-node["crowbar"]["status"]["ipmi"]["user_set"] = false
-node["crowbar"]["status"]["ipmi"]["address_set"] = false
-node.save
+if node["crowbar"]["status"]["ipmi"].nil?
+  node["crowbar"]["status"]["ipmi"] = {}
+  node["crowbar"]["status"]["ipmi"]["user_set"] = false
+  node["crowbar"]["status"]["ipmi"]["address_set"] = false
+  node.save
+end
 
 unsupported = [ "KVM", "Bochs", "VMWare Virtual Platform", "VMware Virtual Platform", "VirtualBox" ]
 
@@ -53,48 +55,54 @@ if node[:ipmi][:bmc_enable]
     return
   end
 
-  node["crowbar"]["status"]["ipmi"]["messages"] = []
-  node.save
+  unless (node["crowbar"]["status"]["ipmi"]["address_set"] and node["crowbar"]["status"]["ipmi"]["user_set"])
+    node["crowbar"]["status"]["ipmi"]["messages"] = []
+    node.save
 
-  ipmi_load "ipmi_load" do
-    settle_time 30
-    action :run
-  end
-  
-  ### lan parameters to check and set. The loop that follows iterates over this array.
-  # [0] = name in "print" output, [1] command to issue, [2] desired value.
-  lan_params = [
-    [ "IP Address Source" ,"ipmitool lan set 1 ipsrc static", "Static Address", 10 ] ,
-    [ "IP Address" ,"ipmitool lan set 1 ipaddr #{bmc_address}", bmc_address, 1 ] ,
-    [ "Subnet Mask" , "ipmitool lan set 1 netmask #{bmc_netmask}", bmc_netmask, 1 ]
-  ]
-
-  lan_params << [ "Default Gateway IP", "ipmitool lan set 1 defgw ipaddr #{bmc_router}", bmc_router, 1 ] unless bmc_router.nil?
-
-  lan_params.each do |param| 
-    ipmi_lan_set "#{param[0]}" do
-      command param[1]
-      value param[2]  
-      settle_time param[3]  
+    ipmi_load "ipmi_load" do
+      settle_time 30
       action :run
     end
   end
+  
+  unless node["crowbar"]["status"]["ipmi"]["address_set"]
+    ### lan parameters to check and set. The loop that follows iterates over this array.
+    # [0] = name in "print" output, [1] command to issue, [2] desired value.
+    lan_params = [
+      [ "IP Address Source" ,"ipmitool lan set 1 ipsrc static", "Static Address", 10 ] ,
+      [ "IP Address" ,"ipmitool lan set 1 ipaddr #{bmc_address}", bmc_address, 1 ] ,
+      [ "Subnet Mask" , "ipmitool lan set 1 netmask #{bmc_netmask}", bmc_netmask, 1 ]
+    ]
 
-  ipmi_user_set "#{bmc_user}" do
-    password bmc_password
-    action :run
+    lan_params << [ "Default Gateway IP", "ipmitool lan set 1 defgw ipaddr #{bmc_router}", bmc_router, 1 ] unless bmc_router.nil?
+
+    lan_params.each do |param| 
+      ipmi_lan_set "#{param[0]}" do
+        command param[1]
+        value param[2]  
+        settle_time param[3]  
+        action :run
+      end
+    end
+
+    bmc_commands = [
+      [ "BMC nic_mode", "/updates/bmc nic_mode set dedicated", "/updates/bmc nic_mode get", "dedicated", 10 ]
+    ]
+
+    bmc_commands.each do |param| 
+      ipmi_bmc_command "bmc #{param[0]}" do
+        command param[1]
+        test param[2]
+        value param[3]
+        settle_time param[4]
+        action :run
+      end
+    end
   end
 
-  bmc_commands = [
-    [ "BMC nic_mode", "/updates/bmc nic_mode set dedicated", "/updates/bmc nic_mode get", "dedicated", 10 ]
-  ]
-
-  bmc_commands.each do |param| 
-    ipmi_bmc_command "bmc #{param[0]}" do
-      command param[1]
-      test param[2]
-      value param[3]
-      settle_time param[4]
+  unless node["crowbar"]["status"]["ipmi"]["user_set"]
+    ipmi_user_set "#{bmc_user}" do
+      password bmc_password
       action :run
     end
   end
