65377697753d148c5e04983a0cb5f7fec42b4bda
Greg Althaus <galthaus@austin.rr.com>
2012-03-20 15:01:39 -0500

    In virtual environmnet, make sure the code will work.

--- chef/cookbooks/ipmi/recipes/ipmi-discover.rb
+++ chef/cookbooks/ipmi/recipes/ipmi-discover.rb
@@ -34,6 +34,9 @@ unsupported = [ "KVM", "Bochs", "VMWare Virtual Platform", "VMware Virtual Platf
 
 if node[:ipmi][:bmc_enable]
   if unsupported.member?(node[:dmi][:system][:product_name])
+    node["crowbar_wall"] = {} unless node["crowbar_wall"]
+    node["crowbar_wall"]["status"] = {} unless node["crowbar_wall"]["status"]
+    node["crowbar_wall"]["status"]["ipmi"] = {} unless node["crowbar_wall"]["status"]["ipmi"]
     node["crowbar_wall"]["status"]["ipmi"]["messages"] = [ "Unsupported platform: #{node[:dmi][:system][:product_name]} - turning off ipmi for this node" ]
     node[:ipmi][:bmc_enable] = false
     node.save
