5b36e317e6d24bc002ea540ef7ef2f6a628a35c2
galthaus <galthaus@austin.rr.com>
2015-01-26 15:35:48 -0600

    Add service function consolidation

--- rails/app/models/barclamp_ntp/service.rb
+++ rails/app/models/barclamp_ntp/service.rb
@@ -13,55 +13,9 @@
 # limitations under the License. 
 # 
 
-class BarclampNtp::Service < Role
+class BarclampNtp::Service < Service
 
-  def do_transition(nr,data)
-    runlog = []
-    addr_arr = []
-    runlog << "Getting ntp-service information from consul"
-    pieces = nil
-    options = {}
-    meta = {}
-    count=0
-    while pieces == nil
-      begin
-        count += 1
-        break if count > 20
-        pieces = Diplomat::Service.get("ntp-service", :all, options, meta)
-        if pieces and pieces.empty?
-          Rails.logger.info("ntp-service not available ... wait 10m or next update")
-          runlog << "ntp-service not available ... wait 10m or next update"
-          if meta[:index]
-            options[:wait] = "10m"
-            options[:index] = meta[:index]
-          else
-            sleep 10
-          end
-          pieces = nil
-        elsif pieces == nil
-          Rails.logger.info("ntp-service not found ... wait 10s")
-          runlog << "ntp-service not found ... wait 10s"
-          sleep 10
-        end
-      rescue Exception => e
-        runlog << "Failed to talk to consul: #{e.message}"
-        Rails.logger.info("Failed to talk to consul: #{e.message}")
-        sleep 10
-      end
-    end
-
-    runlog << "Processing pieces"
-    if pieces
-      pieces.each do |p|
-        addr_arr << p.Address
-      end
-      runlog << "Setting ntp-service attribute"
-      Attrib.set("ntp_servers", nr, addr_arr, :system)
-    end
-
-    Rails.logger.info("Finished waiting for ntp service: #{addr_arr.length} found")
-    nr.runlog = runlog.join("\n")
-    raise "ntp-service not available" if pieces == nil or pieces.empty?
+  def do_transition(nr, data)
+    internal_do_transition(nr, data, "ntp-service", "ntp_servers")
   end
-
 end
