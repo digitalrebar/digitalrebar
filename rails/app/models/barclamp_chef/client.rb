cb0703ac37a842af6095a9facbdfc253ad6fe3db
Rob Hirschfeld <rob@zehicle.com>
2015-01-29 20:35:27 -0800

    do not add chef roles if chef not active (unbreak BDD dev:pop)

--- rails/app/models/barclamp_chef/client.rb
+++ rails/app/models/barclamp_chef/client.rb
@@ -1,10 +1,18 @@
 class BarclampChef::Client < Role
+
+  # create the right private key for chef-client and registers it with the server
   def on_todo(nr)
     # Create chef metadata if needed.
     d = (nr.sysdata["chefjig"]["client"]["key"] rescue nil)
     return if d
     chefjig = Jig.where(:name => "chef").first
     raise "Cannot load Chef Jig" unless chefjig
+    # we have a problem is if the chef jig is not active
+    unless chefjig.active  
+      Rails.logger.warn "Unexpected: Chef Jig should have been active for Chef Client Role to initialize" unless Rails.env.development?
+      return
+    end
+    # creating the node in chef server
     chef_node, chef_role, chef_client = chefjig.create_node(nr.node)
     private_key = nil
     # Sometimes we get an APICilent back, sometimes we get a hash.
