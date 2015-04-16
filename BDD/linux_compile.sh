5305332d103bb3f4abb048caf65e489a0d5d834f
Rob Hirschfeld <rob@zehicle.com>
2015-04-16 13:01:03 -0500

    compile should add config files if missing

--- BDD/linux_compile.sh
+++ BDD/linux_compile.sh
@@ -1,5 +1,12 @@
+
 # Clean-up
 rm -f erl_crash.dump *.beam trace_*.txt
+
+# setup configuration for local use (if missing)
+if [[ ! -f default.config ]]; then
+  cp example.config default.config
+  cp dev.sample dev.config
+fi
 
 # Compile
 erlc +debug_info *.erl
