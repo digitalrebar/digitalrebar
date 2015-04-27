a66a34ad1e596ae82a22e1ddb104ce55c43ccd15
Victor Lowther <victor.lowther@gmail.com>
2015-04-27 10:15:57 -0500

    Fix travis logging issue.

--- rails/config/initializers/delayed_job.rb
+++ rails/config/initializers/delayed_job.rb
@@ -1,1 +1,5 @@
-Delayed::Worker.logger = Logger.new("/var/log/crowbar/delayed_job.log")
+if Rails.env == "production"
+  Delayed::Worker.logger = Logger.new("/var/log/crowbar/delayed_job.log")
+else
+  Delayed::Worker.logger = Logger.new(Rails.root.join("delayed_job.log"))
+end
