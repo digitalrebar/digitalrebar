f51d2769bbaf9036aaa1451c3cb4fd5c99f950cc
Victor Lowther <victor.lowther@gmail.com>
2015-06-05 13:29:37 -0500

    Refactor out test-running code

---
+++ run-tests.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Actually run the BDD, rake, and rspec tests.
+# This assumes that the proper environment has been set up.
+set +e
+run_crowbar() (
+    export CROWBAR_KEY='crowbar:crowbar'
+    cd /opt/opencrowbar/core/rails
+    tasks=("rake db:drop"
+        "rake db:create"
+        "rake railties:install:migrations"
+        "rake db:migrate"
+        "rake db:seed")
+    for task in "${tasks[@]}"; do
+        bundle exec $task
+    done
+    QUE_WORKER_COUNT=2 QUE_QUEUE=HighPriorityRunner bundle exec rake que:work &
+    QUE_WORKER_COUNT=10 QUE_QUEUE=NodeRoleRunner bundle exec rake que:work &
+    bundle exec rails server -d
+    while ! ../bin/crowbar users list; do
+        sleep 1
+    done
+    (cd ..; CROWBAR_KEY="crowbar:crowbar" bin/crowbar barclamps install $PWD)
+)
+
+kill_crowbar() (
+    pkill -f rails
+    pkill -f que:work
+    cd /opt/opencrowbar/core/rails
+    bundle exec rake db:drop
+)
+
+test_bdd() (
+    export RAILS_ENV=development
+    run_crowbar
+    cd /opt/opencrowbar/core/BDD 
+    erlc +debug_info *.erl
+    erl -s bdd test travis -s init stop -noshell
+    local res=$?
+    kill_crowbar
+    return $res
+)
+
+test_rake() (
+    export RAILS_ENV=test
+    cd /opt/opencrowbar/core/rails
+     tasks=("rake db:drop" "rake db:create" "rake railties:install:migrations"
+            "rake db:migrate" "rake db:seed")
+    for task in "${tasks[@]}"; do
+        bundle exec $task
+    done
+    bundle exec rake test && bundle exec rspec
+)
+. /etc/profile
+# Do BDD tests first
+yum -y install erlang
+test_bdd && test_rake
--- test.sh
+++ test.sh
@@ -14,10 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
 date
 
-export RAILS_ENV=test
+export RAILS_ENV=development
 
 # use the host proxy 
 if [[ $http_proxy ]] && ! pidof squid; then
@@ -38,24 +37,4 @@ chef-solo -c /opt/opencrowbar/core/bootstrap/chef-solo.rb -o "${boot_recipes}"
 chef-solo -c /opt/opencrowbar/core/bootstrap/chef-solo.rb -o "${consul_recipes}"
 chef-solo -c /opt/opencrowbar/core/bootstrap/chef-solo.rb -o "${database_recipes}"
 
-. /etc/profile
-
-./setup/00-crowbar-rake-tests.install && \
-  ./setup/01-run-tests.install || {
-  echo "Failed to bootstrap and run tests"
-}
-
-# Talk about tests
-echo
-echo "To run tests:"
-echo "yum -y install erlang"
-echo "su - crowbar"
-echo "cd rails"
-echo "bundle exec rake test"
-echo "bundle exec rspec"
-echo "cd ../BDD"
-echo "./linux_compile.sh"
-echo "./linux_test.sh"
-echo
-
-/bin/bash -i
+exec ./run-tests.sh
