1d5c7a078b3044f3eb7229dad6bae4f0f31690c6
Victor Lowther <victor.lowther@gmail.com>
2015-06-12 13:04:45 -0500

    Make sure vconfig is always part of the Sledgehammer build.
    That skips an otherwise annoying package fetch during Sledgehammer.

--- chef/cookbooks/crowbar-bootstrap/attributes/default.rb
+++ chef/cookbooks/crowbar-bootstrap/attributes/default.rb
@@ -3,7 +3,9 @@ default["bootstrap"]["crowbar_yml"]= "/opt/opencrowbar/core/crowbar.yml"
 default["bootstrap"]["sledgehammer"]["url"] = "http://opencrowbar.s3-website-us-east-1.amazonaws.com/sledgehammer"
 #default["bootstrap"]["sledgehammer"]["signature"] = "0f61af2f6be9288d5529e15aa223e036730a8387"
 # Includes lldpd for link layer switch detection.
-default["bootstrap"]["sledgehammer"]["signature"] = "6bce86d4f5c50f8be7493f2d9ec23075845ea4b9"
+#default["bootstrap"]["sledgehammer"]["signature"] = "6bce86d4f5c50f8be7493f2d9ec23075845ea4b9"
+# Includes vconfig, which I was not including for some derpy reason.
+default["bootstrap"]["sledgehammer"]["signature"] = "69336fba18038fd931b19358eccdc87f40b80c79"
 default["bootstrap"]["tftproot"] = "/tftpboot"
 default["bootstrap"]["gopath"] = "/root/go"
 default["bootstrap"]["gover"] = "1.4"
