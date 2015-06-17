09a79c5bfbcc3973393fd9d0e1fc797b07c2c1df
galthaus <galthaus@austin.rr.com>
2015-06-17 16:59:45 -0500

    Set template dir

--- bind_zones.go
+++ bind_zones.go
@@ -45,7 +45,7 @@ func buildZone(name string, zoneData *ZoneData) Zone {
 
 func NewBindDnsInstance(serverName string) *BindDnsInstance {
 	return &BindDnsInstance{
-		Templates:  template.Must(template.ParseGlob("./*.tmpl")),
+		Templates:  template.Must(template.ParseGlob("/etc/dns_mgmt.d/*.tmpl")),
 		ServerName: serverName,
 	}
 }
