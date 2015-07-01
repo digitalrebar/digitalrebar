ce24bcda2890350a84e39202c83ed870e7e5a4ea
robhirschfeld <rob@zehicle.com>
2015-06-30 23:08:54 -0500

    fix dev script to run crowbar
    Signed-off-by: robhirschfeld <rob@zehicle.com>

--- development.sh
+++ development.sh
@@ -57,6 +57,7 @@ export FQDN
 # we don't run crowbar-config but we do need to do some things from there
 	# Add consul to the default deployment, and make sure it uses the same
 	# acl master token and encryption key as the current running consul.
+cd /opt/opencrowbar/core
 crowbar deployments bind system to consul
 
 for k in acl_master_token encrypt datacenter domain acl_datacenter \
