9438f052d3d8d80157102f30da6d6231ba196d32
robhirschfeld <rob@zehicle.com>
2015-09-07 23:44:37 -0500

    controller working, debugging edge cases

--- rails/app/controllers/barclamps_controller.rb
+++ rails/app/controllers/barclamps_controller.rb
@@ -78,35 +78,60 @@ class BarclampsController < ApplicationController
       @roles = @bc.roles.keep_if { |r| r.milestone }
       @nodes = Deployment.system.nodes.where(:admin=>false, :system=>false)
     elsif request.post?
+
       wiz_name = params[:deployment]
       throw "Deployment Name is required" unless wiz_name
-      d = Deployment.find_or_create_by_name! :name=>wiz_name, :parent=>Deployment.system
 
-      # milestone for OS assignment
-      wiz = Role.find_key 'crowbar-installed-node'
-      #wiz.add_to_deployment d
-      nodes = {}
+      Deployment.transaction do
 
-      # add new roles
-      params.each do |key, value|
-        if key =~ /^node_([0-9]*)_role_([0-9]*)$/
-          n = Node.find $1.to_i
-          r = Role.find $2.to_i
-          nodes[n.id] = params["node_#{n.id}_os"]   # we onlt want to do the node stuff once
-          r.add_to_node_in_deployment n, d
-          Rails.logger.info "Barclamp wizard #{r.name} added node #{n.name}"
+        d = Deployment.find_or_create_by_name! :name=>wiz_name, :parent=>Deployment.system
+
+        # track nodes
+        nodes = {}
+        roles = {}
+        @bc.roles.each { |r| roles[r.id] = [] if r.milestone }  # get the available milestones
+
+        # milestone for OS assignment
+        cin = Role.find_key 'crowbar-installed-node'
+        roles[cin] = []
+
+        # find nodes and roles
+        params.each do |key, value|
+          if key =~ /^node_([0-9]*)_role_([0-9]*)$/
+            nid = $1.to_i
+            rid = $2.to_i
+            nodes[nid] = params["wizard"]["node_#{nid}_os"]   # we only want to do the node stuff once
+            roles[rid] << nid  ## add nodes that we are going to add
+          end
         end
+
+        # set the roles
+        roles.each do |rid, nodes|
+          r = Role.find rid
+          r.add_to_deployment d
+          nodes.each do |nid|
+            n = Node.find nid
+            r.add_to_node_in_deployment(n, d) unless n.is_docker_node? and r.id == cin
+            Rails.logger.info "Barclamp wizard: #{r.name} adding node #{n.name} in deployment #{d.name}"
+          end
+        end
+
+        # set operating systems for selected nodes (unless docker)
+        nodes.each do |nid, os|
+          n = Node.find nid
+          n.deployment = d
+          n.save!
+          next if n.is_docker_node?
+          Attrib.set "provisioner-target_os", n, os, :user
+          Rails.logger.info "Barclamp wizard: assigning #{os} to node #{n.name} in deployment #{d.name}"
+        end
+      
+        redirect_to deployment_path(:id=>d.id)
+
       end
-      # set operating systems for selected nodes (unless docker)
-      nodes.each do |nid, os|
-        n = Node.find nid
-        n.deployment = d
-        n.save!
-        next if n.is_docker_node?
-        Attrib.set "provisioner-target_os", n, os, :user
-      end
-      redirect_to deployment_path(:id=>d.id)
+
     end
+
   end
 
   #
