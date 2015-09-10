b22e526b23a5e5de743a1ea535669c86f306a557
robhirschfeld <rob@zehicle.com>
2015-09-10 13:05:51 -0500

    move production setting into application and move out production.rb so we can make that easily changed

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -26,11 +26,25 @@ end
 
 module Rebar
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.rebar = ActiveSupport::OrderedOptions.new 
-    config.rebar.version = '2.x'
+
+    # defaults are producction
+    config.cache_classes = true
+    config.action_controller.perform_caching             = true
+    config.action_view.cache_template_loading            = true
+    config.active_support.deprecation = :notify
+    config.action_controller.allow_forgery_protection    = true
+    config.eager_load = true
+
+    # See everything in the log (default is :info)
+    config.log_level = :debug
+    config.paths['log'] = "/var/log/rebar/#{Rails.env}.log"
+
+    config.crowbar = ActiveSupport::OrderedOptions.new 
+    config.rebar.version = '2.E'
     config.rebar.docs = "https://github.com/digitalrebar/doc/blob/master/README.rst"
     config.rebar.bootstrap_key = 'digitalrebar/private/bootstrap'
 
