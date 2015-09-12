15726d3c17aa19197c6f7434c7078217548af7e5
Rob Hirschfeld <rob@rackn.com>
2015-09-12 11:35:25 -0500

    make ln -s work

--- compose/workload.sh
+++ compose/workload.sh
@@ -5,7 +5,7 @@ mkdir -p components/rebar_api/digitalrebar
 cd components/rebar_api/digitalrebar
 if [ ! -e $2 ] ; then
   git clone "https://github.com/${1}/${2}.git"
-  ln -s "components/rebar_api/digitalrebar/${2}" $2
+  ln -s "deploy/compose/components/rebar_api/digitalrebar/${2}" "../../../../../${2}"
 else
   cd $2
   git pull
