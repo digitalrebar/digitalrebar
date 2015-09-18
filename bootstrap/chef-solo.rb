1b8c8cb0c65d32eac6e49fd0283b3e5888790b53
Victor Lowther <victor.lowther@gmail.com>
2015-09-18 15:11:11 -0500

    Zap helpers we no longer need since Docker is our install method

--- bootstrap/chef-solo.rb
+++
@@ -1,6 +0,0 @@
-log_level       :info
-log_location    STDOUT
-solo            true
-cookbook_path   "/opt/digitalrebar/core/chef/cookbooks"
-data_bag_path   "/opt/digitalrebar/core/chef/data_bags"
-role_path       "/opt/digitalrebar/core/chef/roles"
--- bootstrap/opensuse.lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-install_prereqs() {
-    zypper -n update
-    zypper -n install curl ruby20 ruby20-devel ruby20-devel-extra
-    zypper -n install \
-           http://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chef-11.16.4-1.el6.x86_64.rpm
-}
