0c675fc3b7a0dd16316077221ff3ada2a01ccac3
galthaus <galthaus@austin.rr.com>
2015-09-18 10:45:22 -0500

    Rearrange the files and dirs to use the cache more and copy less

--- rails/app/models/barclamp_rebar/ansible_playbook_jig.rb
+++ rails/app/models/barclamp_rebar/ansible_playbook_jig.rb
@@ -51,36 +51,35 @@ class BarclampRebar::AnsiblePlaybookJig < Jig
     die "Missing playbook path @ #{role_file}" unless role_yaml['playbook_path']
     die "Missing playbook file @ #{role_file}" unless role_yaml['playbook_file']
 
+    role_map = role_yaml['role_map']
+    role_map = {} unless role_map
+
     # Load/Update cache
     cache_dir = "/var/cache/rebar/ansible_playbook"
     FileUtils.mkdir_p(cache_dir)
+    role_cache_dir = "#{cache_dir}/#{nr.role.name}"
 
-    unless File.exists?("#{cache_dir}/#{nr.role.name}")
+    unless File.exists?("#{role_cache_dir}")
       if role_yaml['playbook_src_path'] =~ /^http/
         # Load the git cache
-        out, err, status = exec_cmd("git clone #{role_yaml['playbook_src_path']} #{cache_dir}/#{nr.role.name}")
+        out, err, status = exec_cmd("git clone #{role_yaml['playbook_src_path']} #{role_cache_dir}")
         die "Failed to get @ #{role_file}: #{out} #{err}" unless status.success?
       else
-        FileUtils.cp_r("#{local_scripts}/#{role_yaml['playbook_src_path']}/.", "#{cache_dir}/#{nr.role.name}")
+        FileUtils.cp_r("#{local_scripts}/#{role_yaml['playbook_src_path']}/.", "#{role_cache_dir}")
       end
     else
       if role_yaml['playbook_src_path'] =~ /^http/
         # Update git repos??
       else
-        FileUtils.cp_r("#{local_scripts}/#{role_yaml['playbook_src_path']}/.", "#{cache_dir}/#{nr.role.name}")
+        FileUtils.cp_r("#{local_scripts}/#{role_yaml['playbook_src_path']}/.", "#{role_cache_dir}")
       end
     end
 
-    remote_tmpdir,err,ok = exec_cmd("mktemp -d /tmp/ansible-playbook-jig-XXXXXX")
-    remote_tmpdir.strip!
-    if remote_tmpdir.empty? || !ok.success?
-      die "Did not create remote_tmpdir for some reason! (#{err})"
+    rundir,err,ok = exec_cmd("mktemp -d /tmp/ansible-playbook-jig-XXXXXX")
+    rundir.strip!
+    if rundir.empty? || !ok.success?
+      die "Did not create rundir for some reason! (#{err})"
     end
-
-    # Make a temp copy of playbook
-    FileUtils.cp_r("#{cache_dir}/#{nr.role.name}", remote_tmpdir)
-
-    rundir = "#{remote_tmpdir}/#{nr.role.name}/#{role_yaml['playbook_path']}"
 
     File.open("#{rundir}/rebar.json","w") do |f|
       JSON.dump(data,f)
@@ -99,17 +98,23 @@ class BarclampRebar::AnsiblePlaybookJig < Jig
         nrs = NodeRole.peers_by_role(nr.deployment, r)
         next if nrs.nil? or nrs.empty?
 
-        # GREG: Map the roles?
-        f.write("[#{r.name}]\n")
-        nrs.each do |tnr|
-          address = tnr.node.address
-          next unless address
-          f.write(address.addr + " ansible_ssh_user=root\n")
+        rns = role_map[r.name]
+        rns = [ r.name ] unless rns
+        rns.each do |rn|
+          next if rn == 'all'
+          f.write("[#{rn}]\n")
+          nrs.each do |tnr|
+            address = tnr.node.address
+            next unless address
+            f.write(address.addr + " ansible_ssh_user=root\n")
+          end
         end
       end
     end
 
-    out,err,ok = exec_cmd("cd #{rundir} ; ansible-playbook -l #{nr.node.address.addr} -i inventory.ini --extra-vars \"@rebar.json\" #{role_yaml['playbook_file']}")
+    rns = role_map[nr.role.name]
+    rns = [ nr.role.name ] unless rns
+    out,err,ok = exec_cmd("cd #{role_cache_dir}/#{role_yaml['playbook_path']} ; ansible-playbook -l #{nr.node.address.addr} -i #{rundir}/inventory.ini --extra-vars \"@#{rundir}/rebar.json\" #{role_yaml['playbook_file']} --tags=#{rns.join(',')}")
     die("Script jig run for #{nr.role.name} on #{nr.node.name} failed! (status = #{$?.exitstatus})\nOut: #{out}\nErr: #{err}") unless ok.success?
     nr.update!(runlog: out)
 
@@ -118,7 +123,7 @@ class BarclampRebar::AnsiblePlaybookJig < Jig
     nr.update!(wall: new_wall)
 
     # Clean up after ourselves.
-    system("rm -rf '#{remote_tmpdir}'")
+    system("rm -rf '#{rundir}'")
   end
 
   def create_node(node)
