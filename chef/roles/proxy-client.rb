1916f7ca9c195c24c271a6b7c5a08b0f0ce1dc56
galthaus <galthaus@austin.rr.com>
2015-09-24 11:42:53 -0500

    Convert proxy client to script jig based so it can before chef-client role.
    Also make the proxy rerun when a nodes networking changes.

--- chef/roles/proxy-client.rb
+++
@@ -1,9 +0,0 @@
-
-name "proxy-client"
-description "Proxy Client Role - Configures the system to point at the proxy server"
-run_list(
-         "recipe[rebar-squid::client]"
-)
-default_attributes()
-override_attributes()
-
---
+++ script/roles/proxy-client/01-setup-proxy.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# Copyright 2015, RackN
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+# See the License for the specific language governing permissions and
+# limitations under the License
+#
+
+proxy_str="$(read_attribute "rebar/proxy/servers/0/url")"
+
+no_proxy="127.0.0.1,localhost,::1,/var/run/docker.sock"
+# Read json array of admin addresses
+admin_addrs=$(read_attribute "proxy/admin_addrs")
+if [ "$admin_addrs" != "" ] ; then
+  admin_addrs=$(echo "$admin_addrs" | jq -r 'map(. + ",") |add')
+  no_proxy="$admin_addrs$no_proxy"
+fi
+# Process ip -o addr show for IP addresses
+ip -o addr show | grep -v inet6 | awk -F/ '{ print $1 }' | awk '{ print $4 }' | while read line ; do
+  no_proxy="$no_proxy,$line"
+done
+
+# look for environment variable
+if [ "$proxy_str" == "" ] ; then
+  proxy_str=$http_proxy
+fi
+
+# Nothing to do
+if [ "$proxy_str" == "" ] ; then
+  exit 0
+fi
+
+cat >/etc/environment <<EOF
+http_proxy=$proxy_str
+https_proxy=$proxy_str
+no_proxy=$no_proxy
+EOF
+
+cp /etc/environment /etc/profile.d/proxy.sh
+
+if [ -e /etc/yum.repos.d ] ; then
+  if [ -e /etc/os-release ] ; then
+    . /etc/os-release
+  else 
+    if [ -e /etc/system-release-cpe ] ; then
+      ID=$(cat /etc/system-release-cpe | awk -F: '{ print $3 }')
+    fi
+  fi
+  cat >/etc/yum.conf <<EOF
+[main]
+cachedir=/var/cache/yum/$basearch/$releasever
+keepcache=0
+debuglevel=2
+logfile=/var/log/yum.log
+exactarch=1
+obsoletes=1
+gpgcheck=1
+plugins=1
+installonly_limit=5
+#bugtracker_url=http://bugs.centos.org/set_project.php?project_id=16&ref=http://bugs.centos.org/bug_report_page.php?category=yum
+distroverpkg=$ID-release
+proxy=$proxy_str
+EOF
+fi
+
