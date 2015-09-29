130484fabbf1051234ab4635ebd88329ea7398bb
Victor Lowther <victor.lowther@gmail.com>
2015-09-29 10:31:08 -0500

    Add more paranioa around the chef client bringup

--- script/roles/chef-client/01-install-client.sh
+++ script/roles/chef-client/01-install-client.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+chef_url="$(read_attribute "chefjig/server/url")"
+chef_client="$(read_attribute "chefjig/client/name")"
+chef_key="$(read_attribute "chefjig/client/key")"
+
+if ! [[ $chef_url && $chef_client && $chef_key ]]; then
+    echo "Missing required attribs!"
+    exit 1
+fi
 
 if [[ -f /etc/os-release ]]; then
   . /etc/os-release
@@ -54,14 +62,12 @@ EOF
 fi
 
 mkdir -p "/etc/chef"
-url=$(read_attribute "chefjig/server/url")
-clientname=$(read_attribute "chefjig/client/name")
 cat > "/etc/chef/client.rb" <<EOF
 log_level       :info
 log_location    STDOUT
-node_name       '$clientname'
-chef_server_url '$url'
+node_name       '$chef_client'
+chef_server_url '$chef_url'
 client_key      '/etc/chef/client.pem'
 EOF
 
-read_attribute "chefjig/client/key" > "/etc/chef/client.pem"
+printf '%s' "$chef_key" > "/etc/chef/client.pem"
