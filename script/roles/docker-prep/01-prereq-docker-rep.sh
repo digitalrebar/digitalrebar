11d7401667b29f90af8be06db62376348273a81b
Rob Hirschfeld <rob@rackn.com>
2015-10-15 15:49:08 -0500

    Docker python API needed for Kolla

--- script/roles/docker-prep/01-prereq-docker-rep.sh
+++ script/roles/docker-prep/01-prereq-docker-rep.sh
@@ -9,6 +9,11 @@ if ! which docker &>/dev/null; then
 else
 	echo "docker already installed, skipping"
 fi
+# install python API
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+python /tmp/get-pip.py
+pip install docker-py
+
 # bounce to be safe
 sudo service docker restart
 # make Docker work without reboot
