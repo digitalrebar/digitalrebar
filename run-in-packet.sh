a69e9f771e3b67e103d2589c96655552f3297d58
Greg Althaus <galthaus@austin.rr.com>
2015-10-28 11:35:25 -0500

    Update packet pieces to run host mode and inject IPs correctly.  Also allow the system to be rerun with a DEVICE_ID.

--- run-in-packet.sh
+++ run-in-packet.sh
@@ -19,10 +19,24 @@ if ! which jq &>/dev/null; then
     exit 1
 fi
 
-NODENAME=$1
-if [ "$NODENAME" == "" ] ; then
-  NODENAME="${USER}1"
+HOST_MODE=""
+if [ "$1" == "--host" ] ; then
+  shift
+  HOST_MODE="YES"
 fi
+
+if [ "$1" == "--device-id" ] ; then
+  shift
+  DEVICE_ID="$1"
+  shift
+else
+  NODENAME=$1
+  if [ "$NODENAME" == "" ] ; then
+    NODENAME="${USER}1"
+    shift
+  fi
+fi
+
 
 TSTAMP=`date +%H%M`
 
@@ -42,7 +56,9 @@ node="{
 
 date
 
-DEVICE_ID=`curl -H "Content-Type: application/json" -X POST --data "$node" -H "X-Auth-Token: $API_KEY" https://api.packet.net/projects/$PROJ_ID/devices | jq -r .id`
+if [ "$DEVICE_ID" == "" ] ; then
+  DEVICE_ID=`curl -H "Content-Type: application/json" -X POST --data "$node" -H "X-Auth-Token: $API_KEY" https://api.packet.net/projects/$PROJ_ID/devices | jq -r .id`
+fi
 
 STATE=`curl -s -H "X-Auth-Token: $API_KEY" https://api.packet.net/projects/$PROJ_ID/devices/$DEVICE_ID | jq -r .state`
 while [ "$STATE" != "active" ] ; do
@@ -55,8 +71,9 @@ date
 
 # Get Public IP - HACK - should look it up
 IP=`curl -s -H "X-Auth-Token: $API_KEY" https://api.packet.net/projects/$PROJ_ID/devices/$DEVICE_ID | jq -r .ip_addresses[0].address`
+CIDR=`curl -s -H "X-Auth-Token: $API_KEY" https://api.packet.net/projects/$PROJ_ID/devices/$DEVICE_ID | jq -r .ip_addresses[0].cidr`
 
-echo "Device ip = $IP"
+echo "Device ip = $IP/$CIDR"
 
 ssh-keygen -f "~/.ssh/known_hosts" -R $IP
 ssh -o StrictHostKeyChecking=no root@$IP date
@@ -64,13 +81,19 @@ ssh -o StrictHostKeyChecking=no root@$IP date
 # Build ansible inventory file
 echo "$IP ansible_ssh_user=root" > run-in-hosts
 
+EXTRA_VARS=""
+if [ "$HOST_MODE" == "YES" ] ; then
+  EXTRA_VARS="dr_access_mode=HOST dr_external_ip=$IP/$CIDR"
+fi
+
 export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -s -i run-in-hosts digitalrebar.yml
+ansible-playbook -i run-in-hosts --extra-vars "$EXTRA_VARS" digitalrebar.yml
 
 date
 
 echo "=== HELPFUL COMMANDS ==="
-echo "repeat Ansible run: ansible-playbook -i run-in-hosts packet.yml"
+echo "Packet Device ID: $DEVICE_ID"
+echo "repeat Ansible run: ansible-playbook -i run-in-hosts --extra-vars \"$EXTRA_VARS\" digitalrebar.yml"
 echo "SSH access: ssh -X root@${IP}"
 echo "Consul UI        http://${IP}:8500"
 echo "Digital Rebar UI http://${IP}:3000"
