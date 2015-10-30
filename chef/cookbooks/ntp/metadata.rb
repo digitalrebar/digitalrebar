24a659206706a4af679e261882b49663cca93143
Victor Lowther <victor.lowther@gmail.com>
2015-10-29 20:05:03 -0500

    Make setting up ntp servers use the script jig instead.

--- chef/cookbooks/ntp/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'ntp'
-maintainer       "Rebar Team"
-maintainer_email "support@rackn.com"
-license          "Apache 2"
-description      "Cookbook for handling NTP for Rebar"
-long_description "Cookbook for handling NTP for Rebar"
-version          "0.9.5"
