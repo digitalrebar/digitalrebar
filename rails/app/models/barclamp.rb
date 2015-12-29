5d87ae9a4ec9a54fa7c0da8b2afbba17ee661906
Greg Althaus <galthaus@austin.rr.com>
2015-12-29 13:44:56 -0600

    Add provider rate for tracking per node per provider rates

--- rails/app/models/barclamp.rb
+++ rails/app/models/barclamp.rb
@@ -221,12 +221,14 @@ class Barclamp < ActiveRecord::Base
         attrib_type = attrib_type_candidates.detect{|at| (at.constantize ? true : false) rescue false}.constantize
         attrib_name = attrib["name"]
         attrib_desc = attrib['description'] || ""
+        attrib_default = attrib['default']
         attrib_map = attrib['map'] || ""
         attrib_writable = !!attrib['schema']
         a = attrib_type.find_or_create_by!(:name => attrib_name)
         a.update_attributes!(:description => attrib_desc,
                              :map => attrib_map,
                              :writable => attrib_writable,
+                             :default => {"value" => attrib_default},
                              :schema => attrib_writable ? attrib['schema']: nil,
                              :barclamp_id => barclamp.id)
       end if bc['attribs']
