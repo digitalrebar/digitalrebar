178ca734787c85a9beafc0413fa20b44b6fdec98
Victor Lowther <victor.lowther@gmail.com>
2016-01-15 11:50:50 -0600

    Allow the DNS management service to be exposed via the forwarder

--- containers/dns/entrypoint.d/50-register-and-start-bind.sh
+++ containers/dns/entrypoint.d/50-register-and-start-bind.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # If we are not using forwarder, we need to use the external address to take to us.
-if [ "$FORWARDER_IP" == "" ] ; then
+if [[ $FORWARDER_IP ]] ; then
+    the_ip=${IP%%/*}
+    cp /root/dns-internal.json /etc/consul.d/dns.json
+    cp /root/dns-mgmt-internal.json /etc/consul.d/dns-mgmt.json
+else
     the_ip=${EXTERNAL_IP%%/*}
     sed -e "s/FILLMEIN/$the_ip/" /root/dns-external.json > /etc/consul.d/dns.json
-else 
-    cp /root/dns-internal.json /etc/consul.d/dns.json
+    sed -e "s/FILLMEIN/$the_ip/" /root/dns-mgmt-external.json > /etc/consul.d/dns-mgmt.json
 fi
 consul reload
 
@@ -15,7 +18,19 @@ consul reload
 ln -s /etc/init.d/bind9 /etc/init.d/named
 service named start
 
+attr="{\"value\": [{
+       \"address\": \"$the_ip\",
+       \"port\": \"6754\",
+       \"name\": \"system\",
+       \"access_name\": \"admin\",
+       \"access_password\": \"admin\",
+       \"url\": \"https://admin:admin@${the_ip}:6754\",
+       \"cert\": $(jq -R -s '@text' </etc/dns-mgmt-https-cert.pem)
+      }]
+}"
 # Make sure we set the token type
+#rebar deployments set system attrib dns-management-servers to "$attr"
+
 curl -X PUT -d 'BIND' http://127.0.0.1:8500/v1/kv/digitalrebar/private/dns/system/type?token=$CONSUL_M_ACL
 
 # Access vars in consul for dns-mgmt
