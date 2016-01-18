d50323b5d8f3469856ad9c17177cd681056c5087
Victor Lowther <victor.lowther@gmail.com>
2016-01-18 11:57:21 -0600

    Make dhcp_servers use rebar attribs, and zap unused KV sets

--- containers/dns/entrypoint.d/50-register-and-start-bind.sh
+++ containers/dns/entrypoint.d/50-register-and-start-bind.sh
@@ -30,10 +30,3 @@ attr="{\"value\": [{
 # Make sure we set the token type
 rebar deployments set system attrib dns-management-servers to "$attr"
 rebar deployments commit system
-
-curl -X PUT -d 'BIND' http://127.0.0.1:8500/v1/kv/digitalrebar/private/dns/system/type?token=$CONSUL_M_ACL
-
-# Access vars in consul for dns-mgmt
-curl -X PUT -d 'admin' http://127.0.0.1:8500/v1/kv/digitalrebar/private/dns-mgmt/system/access_name?token=$CONSUL_M_ACL
-curl -X PUT -d 'admin' http://127.0.0.1:8500/v1/kv/digitalrebar/private/dns-mgmt/system/access_password?$CONSUL_M_ACL
-curl -X PUT --data-binary @/etc/dns-mgmt-https-cert.pem http://127.0.0.1:8500/v1/kv/digitalrebar/private/dns-mgmt/system/cert_pem?token=$CONSUL_M_ACL
