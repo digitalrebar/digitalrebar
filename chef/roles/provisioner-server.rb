3e918d71e5bb075bb9f9ce62ff2c47d00bce1fbf
Victor Lowther <victor.lowther@gmail.com>
2016-01-22 16:27:32 -0600

    Zap provisioner-server role, the provisioner container handles it now.

--- chef/roles/provisioner-server.rb
+++
@@ -1,9 +0,0 @@
-
-name "provisioner-server"
-description "Provisioner Server role - Apt and Networking"
-run_list(
-         "recipe[utils]",
-         "recipe[provisioner::servers]"
-)
-default_attributes()
-override_attributes()
