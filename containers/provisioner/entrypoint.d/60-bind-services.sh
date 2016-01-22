e0f66be85428777b91b6b4f69407c6e543e13be1
Victor Lowther <victor.lowther@gmail.com>
2016-01-22 16:22:15 -0600

    Make container responsible for all the rest of provisioner server setup

--- containers/provisioner/entrypoint.d/60-bind-services.sh
+++ containers/provisioner/entrypoint.d/60-bind-services.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if ! rebar nodes roles "$HOSTNAME" |grep -q 'provisioner-base-images'; then
-     rebar nodes bind "$HOSTNAME" to 'provisioner-server'
      rebar nodes bind "$HOSTNAME" to 'provisioner-database'
      rebar nodes bind "$HOSTNAME" to 'provisioner-base-images'
 fi
