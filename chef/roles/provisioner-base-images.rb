267a61bd5fd99687e67574b04c45980e049e4057
Victor Lowther <victor.lowther@gmail.com>
2016-01-26 17:04:57 -0600

    Shift responsibility for setting up base images to the provisioner container.

--- chef/roles/provisioner-base-images.rb
+++
@@ -1,9 +0,0 @@
-
-name "provisioner-base-images"
-description "Provisioner Server role - Apt and Networking"
-run_list(
-         "recipe[utils]",
-         "recipe[provisioner::setup_base_images]"
-)
-default_attributes()
-override_attributes()
