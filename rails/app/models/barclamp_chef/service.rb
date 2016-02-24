421d16efe45dc63d338036dd417da6cdc4fe8245
Victor Lowther <victor.lowther@gmail.com>
2016-02-24 08:33:41 -0600

    Shift responsibility for Chef bringup to the rebar-api container

--- rails/app/models/barclamp_chef/service.rb
+++ rails/app/models/barclamp_chef/service.rb
@@ -20,39 +20,23 @@ class BarclampChef::Service < Service
     wait_for_service(nr, data, "chef-service")
     deployment_role = nr.deployment_role
     admin_accounts=nil
-    admin_keys=nil
     chef_servers = nil
-    until admin_accounts && admin_keys && chef_servers do
+    until admin_accounts && chef_servers do
       admin_accounts = Attrib.get('chef-servers-admin-name',deployment_role)
-      admin_keys = Attrib.get('chef-servers-admin-key',deployment_role)
       chef_servers = Attrib.get('chef-servers',deployment_role)
       Rails.logger.info("chef-service: Testing required attribs")
-      unless admin_accounts && admin_keys && chef_servers
+      unless admin_accounts && chef_servers
         deployment_role.reload
         sleep 1
       end
     end
     account = admin_accounts.first
     url = chef_servers.first
-    key = admin_keys.first
     # Update knife file and key file
-    Rails.logger.info("chef-service: Writing knife client information and key")
-    knife_contents = "
-log_level                :info
-log_location             STDOUT
-node_name                '#{account}'
-client_key               '/home/rebar/.chef/#{account}.pem'
-chef_server_url          '#{url}'
-syntax_check_cache_path  '/home/rebar/.chef/syntax_check_cache'
-ssl_verify_mode          :verify_none
-"
-    system("mkdir -p /home/rebar/.chef")
-    File.open("/home/rebar/.chef/knife.rb", 'w', 0600) {|f| f.write(knife_contents) }
-    File.open("/home/rebar/.chef/#{account}.pem", 'w', 0600) {|f| f.write(key) }
-
-    # Make sure chef-server has code
-    Rails.logger.info("chef-service: Uploading initial cookbooks to chef server")
-    system("/opt/digitalrebar/core/bin/chef-cookbook-upload >/tmp/chef-upload.out 2>&1")
+    until File.exist?("/home/rebar/.chef/knife.rb") &&
+          File.exist?("/home/rebar/.chef/#{account}.pem")
+      sleep 1
+    end
 
     j = BarclampChef::Jig.where(:name => "chef").first
     j.server = url
