8c537237a06a91d1c948522c9040097dbc3e6e50
Greg Althaus <galthaus@austin.rr.com>
2016-02-25 07:36:32 -0800

    Allow DNS to be external for NSUPDATE and POWERDNS. This will push that IP into the dns_servers entry.

--- containers/dns/entrypoint.d/50-register-and-start-bind.sh
+++ containers/dns/entrypoint.d/50-register-and-start-bind.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
-# If we are not using forwarder, we need to use the external address to take to us.
+# The IP to expose
 the_ip=${EXTERNAL_IP%%/*}
-make_service "dns" "53" '{"script": "dig @127.0.0.1 127.0.0.1 >/dev/null 2>&1", "interval": "10s"}'
+
+# If we aren't bind, make that the server to talk to.
+dns_ip=127.0.0.1
+if [[ $DNS_TYPE != BIND ]] ; then
+    dns_ip=$DNS_SERVER
+fi
+
+# If we are not using forwarder, we need to use the external address to take to us.
+make_service "dns" "53" "{\"script\": \"dig @$dns_ip $dns_ip >/dev/null 2>&1\", \"interval\": \"10s\"}"
+
+# Start the services.
+if [[ $DNS_TYPE == BIND ]] ; then
+    /usr/sbin/named -g -u bind &
+fi
+
+bind_service dns-service
+
+# Make sure we set the token type
+
+# If we aren't bind, make that the server to talk to.
+dns_ip=$the_ip
+if [[ $DNS_TYPE != BIND ]] ; then
+    dns_ip=$DNS_SERVER
+fi
+
+set_service_attrib dns-service dns-domain "{\"value\": \"$BASE_DOMAINNAME\"}"
+set_service_attrib dns-service dns_servers \
+"{\"value\":[
+    {\"address\": \"$dns_ip\",
+     \"port\": \"53\",
+     \"name\": \"$SERVICE_DEPLOYMENT\"}]}"
+
+
+# Set up the management service
 make_service "dns-mgmt" "6754" '{ "script": "pidof rebar-dns-mgmt","interval": "10s"}'
 
 # Append DNS mgmt config from env vars
@@ -15,14 +48,8 @@ type = $DNS_TYPE
 server = $DNS_SERVER
 $OTHER_PARMS
 EOF
-
-# Start the services.
 /usr/local/bin/rebar-dns-mgmt --backing_store=consul --data_dir=digitalrebar/dns/database &
-if [[ $DNS_TYPE == BIND ]] ; then
-    /usr/sbin/named -g -u bind &
-fi
 
-bind_service dns-service
 bind_service dns-mgmt_service
 
 attr="{\"value\": [{
@@ -35,11 +62,5 @@ attr="{\"value\": [{
        \"cert\": $(jq -R -s '@text' </etc/dns-mgmt-https-cert.pem)
       }]
 }"
-# Make sure we set the token type
-set_service_attrib dns-service dns-domain "{\"value\": \"$BASE_DOMAINNAME\"}"
 set_service_attrib dns-mgmt_service dns-management-servers "$attr"
-set_service_attrib dns-service dns_servers \
-"{\"value\":[
-    {\"address\": \"$the_ip\",
-     \"port\": \"53\",
-     \"name\": \"$SERVICE_DEPLOYMENT\"}]}"
+
