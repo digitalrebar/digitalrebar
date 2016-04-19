3a3b9a1532b22a0dae294988a268677d1d0cd766
Greg Althaus <galthaus@austin.rr.com>
2016-04-18 21:43:23 -0500

    Add a delay action that will run actions after a period of time.

--- action.go
+++ action.go
@@ -41,6 +41,44 @@ func actionScript(val interface{}) (Action, error) {
 	}, nil
 }
 
+func actionDelay(val interface{}) (Action, error) {
+	array, ok := val.([]interface{})
+	if !ok {
+		return nil, errors.New("Delay needs nested actions")
+	}
+	if len(array) < 2 {
+		return nil, errors.New("Delay needs at least one nested action and a duration")
+	}
+
+	jj := array[0].(map[string]interface{})
+
+	// First element must be a map with duration integer
+	dobj, ok := jj["Duration"]
+	if !ok {
+		return nil, errors.New("Delay needs a map with duration as first element")
+	}
+	duration, ok := dobj.(float64)
+	if !ok {
+		return nil, errors.New("Duration must contain an integer")
+	}
+
+	actions := make([]Action, 0, 0)
+	// The remaining elements must compile to actions
+	for _, e := range array[1:] {
+		element := e.(map[string]interface{})
+		subaction, err := ResolveAction(element)
+		if err != nil {
+			return nil, err
+		}
+		actions = append(actions, subaction)
+	}
+
+	return func(e *RunContext) error {
+		delay_actions(e, int(duration), actions)
+		return nil
+	}, nil
+}
+
 // ResolveAction compiles a map[string]interface{} with a single key-value pair into a function matching
 // the Action type.  Recognized keys are:
 //
@@ -50,6 +88,8 @@ func actionScript(val interface{}) (Action, error) {
 // "Script", which expects its value to be a string that will be parsed using text.Template.  The result of that parsing should
 // be a valid bash script.  When the Action is ran, the parsed script will be compiled using the passed RunContext.  If the compilation fails,
 // or the resultant script executes with a non-zero exit status, the Action will fail, otherwise it will pass.
+//
+// "Delay", which causes the a delay of duration seconds for doing the encapsulated actions described above.
 func ResolveAction(a map[string]interface{}) (Action, error) {
 	if len(a) != 1 {
 		return nil, fmt.Errorf("Actions have exactly one key")
@@ -60,6 +100,8 @@ func ResolveAction(a map[string]interface{}) (Action, error) {
 			return actionLog()
 		case "Script":
 			return actionScript(v)
+		case "Delay":
+			return actionDelay(v)
 		default:
 			return nil, fmt.Errorf("Unknown action %s", t)
 		}
