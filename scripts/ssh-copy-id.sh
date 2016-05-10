6665f883bc452bf428621db1a001e81aed2fefbe
Greg Althaus <galthaus@austin.rr.com>
2016-05-10 10:12:57 -0500

    Make admin node aws disks disappear on termination. Fix echo issue with backticks.

--- scripts/ssh-copy-id.sh
+++ scripts/ssh-copy-id.sh
@@ -93,7 +93,7 @@ fi
 
 ID=$(eval $GET_ID)
 
-echo "To resolve `unable to resolve host name...` add hostname to /etc/hosts on remote system"
+echo "To resolve 'unable to resolve host name...' add hostname to /etc/hosts on remote system"
 
 echo "You may be asked for $NODE_INFO password"
 ssh -tt $INIT_ID_FILE $NODE_INFO "umask 077; $SUDO test -d ${DIR_TO_CHANGE} || $SUDO mkdir ${DIR_TO_CHANGE} ; $SUDO sh -c \"echo \\\"$ID\\\" >> ${FILE_TO_CHANGE}\"; $CLEAN_IT $SUDO sort -u ${FILE_TO_CHANGE} > /tmp/a ; $SUDO cp /tmp/a ${FILE_TO_CHANGE} ; $SUDO rm /tmp/a ; $SUDO test -x /sbin/restorecon && $SUDO /sbin/restorecon ${DIR_TO_CHANGE} ${FILE_TO_CHANGE}" || exit 1
