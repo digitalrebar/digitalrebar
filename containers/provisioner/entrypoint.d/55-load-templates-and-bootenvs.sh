d2a5503febedcacc7d9c51d9c978f67f71b4cb53
Victor Lowther <victor.lowther@gmail.com>
2016-05-23 09:41:52 -0500

    Have the provisioner container pull provisioner-mgmt instead of building it

--- containers/provisioner/entrypoint.d/55-load-templates-and-bootenvs.sh
+++ containers/provisioner/entrypoint.d/55-load-templates-and-bootenvs.sh
@@ -3,13 +3,13 @@
 ACCESS_ARGS="--cacert /etc/prov-base-cert.pem --cert /etc/prov-cert.pem --key /etc/prov-key.pem"
 
 # Load templates
-for f in "$GOPATH/src/github.com/rackn/provisioner-mgmt/templates"/*.tmpl; do
+for f in "/opt/provisioner-mgmt/templates"/*.tmpl; do
     curl $ACCESS_ARGS -X POST --data-binary "@$f" \
          "https://localhost:$APIPORT/templates/${f##*/}" || :
 done
 
 # Load bootenvs
-for f in "$GOPATH/src/github.com/rackn/provisioner-mgmt/bootenvs"/*.json; do
+for f in "/opt/provisioner-mgmt/bootenvs"/*.json; do
     curl $ACCESS_ARGS -X POST --data-binary "@$f" \
          -H 'Content-Type: application/json' \
          https://localhost:$APIPORT/bootenvs || :
--- containers/provisioner/update-nodes/error.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type pkgError struct {
-	Message string
-}
-
-func NewError(s string) *pkgError {
-	return &pkgError{Message: s}
-}
