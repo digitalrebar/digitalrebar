b06371ebac7687e54b52f818ea46cf3ad9abeee1
Victor Lowther <victor.lowther@gmail.com>
2016-07-07 15:37:45 -0500

    Add datastore for saving/loading data from the engine.

---
+++ engine/store.go
@@ -0,0 +1,81 @@
+package engine
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/hashicorp/consul/api"
+)
+
+type LoadSaver interface {
+	Save([]byte) error
+	Load() ([]byte, error)
+}
+
+type FileStore struct {
+	backingDatabase string
+}
+
+func NewFileStore(dbFile string) (*FileStore, error) {
+	info, err := os.Stat(dbFile)
+	if os.IsNotExist(err) {
+		err = ioutil.WriteFile(dbFile, []byte("{}"), 0600)
+	}
+	if err != nil {
+		return nil, err
+	}
+	if !info.Mode().IsRegular() {
+		return nil, fmt.Errorf("%s is not a regular file", dbFile)
+	}
+	return &FileStore{backingDatabase: dbFile}, nil
+}
+
+func (fs *FileStore) Save(buf []byte) error {
+	return ioutil.WriteFile(fs.backingDatabase, buf, 0600)
+}
+
+func (fs *FileStore) Load() ([]byte, error) {
+	return ioutil.ReadFile(fs.backingDatabase)
+}
+
+type ConsulStore struct {
+	store      *api.KV
+	backingKey string
+}
+
+func NewConsulStore(key string) (*ConsulStore, error) {
+	client, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		return nil, err
+	}
+	if _, err := client.Agent().Self(); err != nil {
+		return nil, err
+	}
+
+	store := client.KV()
+	pair, _, err := store.Get(key, nil)
+	if err != nil {
+		return nil, err
+	}
+	if pair == nil {
+		_, err := store.Put(&api.KVPair{Key: key, Value: []byte("{}")}, nil)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &ConsulStore{store: store, backingKey: key}, nil
+}
+
+func (cs *ConsulStore) Load() ([]byte, error) {
+	pair, _, err := cs.store.Get(cs.backingKey, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+	return pair.Value, err
+}
+
+func (cs *ConsulStore) Save(buf []byte) error {
+	_, err := cs.store.Put(&api.KVPair{Key: cs.backingKey, Value: buf}, nil)
+	return err
+}
