0abb80e5a644109f814aa359559ab36ac5dead3e
Greg Althaus <galthaus@austin.rr.com>
2016-07-20 12:41:19 -0500

    Add helper functions to start TLS services that build things from trust-me and consul.  Add a get service wait service helper as well.

---
+++ service/service.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"time"
+
+	"github.com/hashicorp/consul/api"
+)
+
+func GetService(client *api.Client, name, tag string) ([]*api.CatalogService, error) {
+	catalog := client.Catalog()
+	q := api.QueryOptions{
+		WaitIndex: 0,
+		WaitTime:  time.Second * 10,
+	}
+	cs, _, err := catalog.Service(name, tag, &q)
+	return cs, err
+}
+
+func WaitService(client *api.Client, name, tag string) ([]*api.CatalogService, error) {
+	catalog := client.Catalog()
+	q := api.QueryOptions{
+		WaitIndex: 0,
+		WaitTime:  time.Second * 10,
+	}
+	for true {
+		cs, qm, err := catalog.Service(name, tag, &q)
+		if err != nil {
+			return nil, err
+		}
+		if len(cs) > 0 {
+			return cs, nil
+		}
+		q.WaitIndex = qm.LastIndex
+	}
+	return []*api.CatalogService{}, nil
+}
