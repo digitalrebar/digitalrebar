59724d0c35fdbbf2426b1552d82bcf6a367f0a9a
Greg Althaus <galthaus@austin.rr.com>
2016-07-22 22:55:10 -0500

    Only require getting the key for sign.  This keeps from haning for 2 minutes when create roots.

--- sign-it/main.go
+++ sign-it/main.go
@@ -11,7 +11,26 @@ import (
 	"strings"
 
 	"github.com/digitalrebar/go-common/cert"
+	"github.com/digitalrebar/go-common/service"
+	consul "github.com/hashicorp/consul/api"
 )
+
+func getTrustMeServiceAddress() (string, error) {
+	cc, err := consul.NewClient(consul.DefaultConfig())
+	if err != nil {
+		return "", err
+	}
+	if _, err := cc.Agent().Self(); err != nil {
+		return "", err
+	}
+
+	svc, err := service.WaitService(cc, "trust-me-service", "")
+	if err != nil {
+		log.Printf("Could not get trust-me service: %v\n", err)
+		return "", err
+	}
+	return fmt.Sprintf("https://%s:%d", svc[0].ServiceAddress, svc[0].ServicePort), nil
+}
 
 func main() {
 	flagAuto := flag.Bool("A", false, "Use Consul to set address and key of trustme")
@@ -48,16 +67,15 @@ func main() {
 		log.Fatalf("Must specify at least one of Make, Sign, or Info")
 	}
 
-	if *flagAuto {
-		address, buffer, err := cert.GetTrustMeServiceInfo(*flagLabel)
-		if err != nil {
-			log.Fatal("Failed to get consul data: ", err)
-		}
-		*flagKey = string(buffer)
-		*flagAddr = address
-	}
-
 	if *flagMakeRoot {
+		if *flagAuto {
+			address, err := getTrustMeServiceAddress()
+			if err != nil {
+				log.Fatal("Failed to get consul data: ", err)
+			}
+			*flagAddr = address
+		}
+
 		url := *flagAddr + "/api/v1/cfssl/root"
 		kstring := ""
 		if *flagKey != "" {
@@ -82,6 +100,14 @@ func main() {
 	}
 
 	if *flagInfo {
+		if *flagAuto {
+			address, err := getTrustMeServiceAddress()
+			if err != nil {
+				log.Fatal("Failed to get consul data: ", err)
+			}
+			*flagAddr = address
+		}
+
 		cacert, err := cert.GetCertificateForRoot(*flagAddr, *flagLabel)
 		if err != nil {
 			log.Printf("Failed to get Certificate for label: %s: %v\n", *flagLabel, err)
@@ -99,6 +125,14 @@ func main() {
 
 	if *flagSign {
 		hosts := strings.Split(*flagHosts, ",")
+		if *flagAuto {
+			address, buffer, err := cert.GetTrustMeServiceInfo(*flagLabel)
+			if err != nil {
+				log.Fatal("Failed to get consul data: ", err)
+			}
+			*flagKey = string(buffer)
+			*flagAddr = address
+		}
 
 		mycert, mykey, err := cert.CreateCertificate(*flagAddr, *flagKey, *flagLabel, *flagCN, hosts)
 		if err != nil {
