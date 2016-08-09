f532215540dea565800d64b9f3f9f2919204727f
Victor Lowther <victor.lowther@gmail.com>
2016-08-09 09:38:20 -0500

    Add a Consul service finding helper.

--- service/service.go
+++ service/service.go
@@ -34,3 +34,31 @@ func WaitService(client *api.Client, name, tag string) ([]*api.CatalogService, e
 	}
 	return []*api.CatalogService{}, nil
 }
+
+// Look for a Rebar registered service
+// It looks for service names in the following order:
+//    - internal-name-service
+//    - name-service
+//    - name
+func Find(client *api.Client, name, tag string) ([]*api.CatalogService, error) {
+	catalog := client.Catalog()
+	q := api.QueryOptions{
+		WaitIndex: 0,
+		WaitTime:  time.Second * 10,
+	}
+	names := []string{
+		"internal-" + name + "-service",
+		name + "-service",
+		name,
+	}
+	for i := range names {
+		s, _, err := catalog.Service(names[i], tag, &q)
+		if err != nil {
+			return nil, err
+		}
+		if len(s) > 0 {
+			return s, nil
+		}
+	}
+	return []*api.CatalogService{}, nil
+}
