7278e69ea47ff74ded99305fe28eba131713daee
Victor Lowther <victor.lowther@gmail.com>
2016-08-15 11:40:03 -0500

    Add little trusted Rebar client from Consul helper package.

---
+++ client/trusted.go
@@ -0,0 +1,46 @@
+// Package client is where you can easily create Rebar clients.
+// For now we only handle creating trusted clients.
+package client
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/digitalrebar/go-common/service"
+	"github.com/digitalrebar/rebar-api/api"
+	consul "github.com/hashicorp/consul/api"
+)
+
+// Trusted creates a fully preconfigured trusted Rebar API client based on information in Consul.
+// If wait is true, this function will wait until the Rebar API is registered in Consul
+func Trusted(user string, wait bool) (*api.Client, error) {
+	var cClient *consul.Client
+	var err error
+	for {
+		cClient, err = consul.NewClient(consul.DefaultConfig())
+		if err == nil {
+			break
+		}
+		if !wait {
+			return nil, fmt.Errorf("RebarClient: Error getting local Consul client: %v", err)
+		}
+		time.Sleep(10 * time.Second)
+	}
+	for {
+		apiService, err := service.Find(cClient, "rebar-api", "")
+		if err != nil {
+			return nil, fmt.Errorf("RebarClient: Error talking to local Consul: %v", err)
+		}
+		if len(apiService) == 0 {
+			if wait {
+				log.Println("Rebar API service not registered yet")
+				time.Sleep(10 * time.Second)
+				continue
+			}
+			return nil, fmt.Errorf("RebarClient: Rebar API not registered with Consul")
+		}
+		apiAddr, apiPort := service.Address(apiService[0])
+		return api.TrustedSession(fmt.Sprintf("https://%s:%d", apiAddr, apiPort), user)
+	}
+}
