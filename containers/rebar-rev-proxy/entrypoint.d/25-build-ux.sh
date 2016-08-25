c2dab854ae81a15c80f75036f8edbaa45d85e9c8
Greg Althaus <galthaus@austin.rr.com>
2016-08-25 12:02:14 -0500

    Put back the build pieces for all the pieces.

--- containers/rebar-rev-proxy/entrypoint.d/25-build-ux.sh
+++ containers/rebar-rev-proxy/entrypoint.d/25-build-ux.sh
@@ -4,6 +4,9 @@
 cd /opt/digitalrebar-ux
 if [[ $REVPROXY_REBUILD_BOWER ]] ; then
   bower --allow-root install --config.interactive=false
+  npm install cssnano-cli html-minifier uglify-js
+  npm install -g n
+  n stable
 fi
 ./build.sh
 cd -
