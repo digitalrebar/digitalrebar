c232dc265abad91983baf2560cefcf14d8e6ad78
Victor Lowther <victor.lowther@gmail.com>
2016-08-30 08:52:39 -0500

    Add a rebar helper that uses trusted authentication.
    This gets rid of most of the hardcoding for the rebar user.

--- containers/rebar-api/entrypoint.d/23-wait-for-api-to-start.sh
+++ containers/rebar-api/entrypoint.d/23-wait-for-api-to-start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-while ! rebar -U rebar -P rebar1 ping; do
-    sleep 5
-done
-
  
 # Add rev-proxy matcher
 OSD=$SERVICE_DEPLOYMENT
@@ -11,5 +6,6 @@ SERVICE_DEPLOYMENT="$OSD\", \"revproxy-default"
 make_revproxied_service "rebar-api" 3000 '{"script": "rebar -T -E $REBAR_ENDPOINT -U system ping","interval": "10s"}'
 SERVICE_DEPLOYMENT=$OSD
 
-
-
+while ! rebar ping; do
+    sleep 5
+done
--- containers/rebar-api/entrypoint.d/24-build-initial-machine-keys.sh
+++ containers/rebar-api/entrypoint.d/24-build-initial-machine-keys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build initial access keys
-if ! rebar -U rebar -P rebar1 users show machine-install; then
+if ! rebar users show machine-install; then
     echo "Creating machine-install user"
     MACHINE_PASSWORD=$(dd if=/dev/urandom bs=64 count=1 2>/dev/null | sha512sum - 2>/dev/null | awk '{ print $1 }')
     machine_user="
@@ -15,7 +15,7 @@ if ! rebar -U rebar -P rebar1 users show machine-install; then
   \"digest\": true
 }"
 
-    if ! rebar -U rebar -P rebar1 users import "$machine_user"; then
+    if ! rebar users import "$machine_user"; then
         echo "Could not create machine-install user!"
         exit 1
     fi
@@ -23,7 +23,7 @@ if ! rebar -U rebar -P rebar1 users show machine-install; then
     for cap in NODE_READ ROLE_READ NODE_CREATE NODE_UPDATE NODE_COMMIT \
                          DEPLOYMENT_READ DEPLOYMENT_UPDATE PROVIDER_READ; do
         blob="{\"user_id\":\"machine-install\",\"tenant_id\":\"system\",\"capability_id\":\"$cap\"}"
-        rebar -U rebar -P rebar1 user_tenant_capabilitys create "$blob"
+        rebar user_tenant_capabilitys create "$blob"
     done
 
     echo "machine-install:${MACHINE_PASSWORD}" >/etc/rebar.install.key
