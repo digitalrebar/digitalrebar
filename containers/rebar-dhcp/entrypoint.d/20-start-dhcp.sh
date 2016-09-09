7ade5c893dfa4c911cdb4a984cf388f7e38f525e
Greg Althaus <galthaus@austin.rr.com>
2016-09-09 16:36:03 -0500

    Remove gcfg parts of DHCP and DNS containers

--- containers/rebar-dhcp/entrypoint.d/20-start-dhcp.sh
+++ containers/rebar-dhcp/entrypoint.d/20-start-dhcp.sh
@@ -7,6 +7,8 @@ run_forever() (
   done
 )
 
+DHCP_MGMT_PORT=${DHCP_MGMT_PORT:-6755}
+
 HOSTS=`hostname`
 HOSTS="dhcp,dhcp-mgmt,dhcp-mgmt-service,$HOSTS,localhost"
 I_IP=`ip addr show | grep inet | grep -v inet6 | awk '{ print $2 }' | awk -F/ '{ print $1 }'|sed "s/ /,/g"`
@@ -14,8 +16,7 @@ CI_IP=`echo $I_IP | sed "s/ /,/g"`
 HOSTS="$HOSTS,$CI_IP"
 
 # Service has to start first - or we should move the consul stuff into the app.
-run_forever /usr/local/bin/rebar-dhcp --server_ip=$EXTERNAL_IP --backing_store=consul --data_dir=digitalrebar/dhcp/database --auth_mode=KEY --host="$HOSTS" &
+run_forever /usr/local/bin/rebar-dhcp --port $DHCP_MGMT_PORT --serverIp=$EXTERNAL_IP --backingStore=consul --dataDir=digitalrebar/dhcp/database --host="$HOSTS" &
 make_service dhcp 67 '{"script": "pidof rebar-dhcp","interval": "10s"}'
-make_revproxied_service dhcp-mgmt  6755 '{"script": "pidof rebar-dhcp","interval": "10s"}'
-
+make_revproxied_service dhcp-mgmt  $DHCP_MGMT_PORT '{"script": "pidof rebar-dhcp","interval": "10s"}'
 
