23674d4a29713d528837cd714d37794248713cdb
Greg Althaus <galthaus@austin.rr.com>
2016-09-13 22:03:24 -0500

    Silly shell expansion.

--- go/build-rebar.sh
+++ go/build-rebar.sh
@@ -3,13 +3,13 @@
 set -e
 # Requires GOPATH to be set and will use it
 
-OLDPATH=/Users/galthaus/new-dig/digitalrebar/go
-cd /Users/galthaus/go_stuff
+OLDPATH=`pwd`
+cd $GOPATH
 
 go get github.com/rackn/digitalrebar/go
 rm -f bin/go
 go build -o build-rebar github.com/rackn/digitalrebar/go
 ./build-rebar 
 
-cd 
+cd $OLDPATH
 
