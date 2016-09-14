67357363c2e1e8e087161937e4739c4137328daa
Victor Lowther <victor.lowther@gmail.com>
2016-09-14 14:35:37 -0500

    Update build-rebar.sh to be a little more hygenic about what it pulls.

--- go/build-rebar.sh
+++ go/build-rebar.sh
@@ -1,15 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 # Requires GOPATH to be set and will use it
 
-OLDPATH=`pwd`
-cd $GOPATH
+if [[ $PWD != */src/github.com/digitalrebar/digitalrebar/go ]]; then
+    cd "${GOPATH%%:*}"
+    go get -d github.com/digitalrebar/digitalrebar/go
+    cd src/github.com/digitalrebar/digitalrebar/go
+fi
 
-go get github.com/digitalrebar/digitalrebar/go
-rm -f bin/go
-go build -o build-rebar github.com/digitalrebar/digitalrebar/go
-./build-rebar 
+mkdir -p vendor_src
+(cd vendor_src && ln -sf ../vendor src)
 
-cd $OLDPATH
+glide i
 
+binversion="$(go run version.go)"
+
+arches=("amd64")
+oses=("linux" "darwin")
+packages=("github.com/digitalrebar/digitalrebar/go/certificates/sign-it"
+	  "github.com/digitalrebar/digitalrebar/go/certificates/trust-me"
+	  "github.com/digitalrebar/digitalrebar/go/rebar-dhcp"
+	  "github.com/digitalrebar/digitalrebar/go/rebar-dns-mgmt"
+	  "github.com/digitalrebar/digitalrebar/go/rule-engine"
+	  "github.com/digitalrebar/digitalrebar/go/rebar-rev-proxy"
+	  "github.com/digitalrebar/digitalrebar/go/rebar-api/rebar"
+	  "github.com/digitalrebar/digitalrebar/go/forwarder"
+	  "github.com/digitalrebar/digitalrebar/go/provisioner-mgmt")
+
+for arch in "${arches[@]}"; do
+    for os in "${oses[@]}"; do
+        binpath="bin/$binversion/$os/$arch"
+        mkdir -p "$binpath"
+        for pkg in "${packages[@]}"; do
+            GOOS="$os" GOARCH="$arch" go build -o "${binpath}/${pkg##*/}" "$pkg"
+        done
+    done
+done
+
+cd "bin/$binversion"
+find . -type f -perm -u=x |xargs sha256sum >sha256sums
---
+++ go/version.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/digitalrebar/digitalrebar/go/common/version"
+)
+
+func main() {
+	fmt.Printf("%s\n", version.REBAR_VERSION)
+}
