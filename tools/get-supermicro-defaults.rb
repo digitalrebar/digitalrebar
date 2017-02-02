31dfa582bc1c3409a929ae690e2d2837ac8cc437
Victor Lowther <victor.lowther@gmail.com>
2017-02-02 09:13:39 -0600

    Add default BIOS setting fetchers for Dell and Supermicro gear

--- tools/get-supermicro-defaults.rb
+++ tools/get-supermicro-defaults.rb
@@ -1,15 +1,23 @@
-res = Hash.new
+#!/usr/bin/env ruby
+def log(str)
+  $stderr.puts(str)
+end
+
+unless File.exists?("sum_bios")
+  log "Please run /path/to/sum -i example.system.ipmi.address -u user -p password -c GetDefaultBiosCfgTextTile --file sum_bios"
+  log "This will get the default BIOS settings from the target SuperMicro system"
+  log " For more information, read the manual for the SuperMicro Update Manager"
+  exit 1
+end
 lines = IO.readlines("sum_bios")
 header_re=/^\[(.+)\]$/
 setting_re=/^([^=]+)=([0-9a-fA-F]+)/
-option_re=/\*?(([0-9a-fA-F]+) \(.+)/
-default_enum_re=/\*(([0-9a-fA-F]+) \(.+)/
 default_val_re=/Default value is <<<([0-9a-fA-F]+)>>>/
 header = ''
+res = Hash.new
 lines.each do |l|
   line = l.strip
   next if line.empty? || line[0] == '#'
-  i = Hash.new
   case
   when header_re =~ line
     header = $~[1]
@@ -23,6 +31,9 @@ lines.each do |l|
       default = values.select{|v|v[0] == "*"}.first[1..-1]
       default << ")" if default[-1] != ")"
     end
-    puts "\"#{name}\": \"#{default}\""
+    res[name] = default
   end
 end
+
+require 'json'
+puts JSON.pretty_generate({"default" => res})
