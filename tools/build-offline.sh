29ede7755dd118cc6b5d161937dac4959e5b3f76
Victor Lowther <victor.lowther@gmail.com>
2017-05-15 15:32:32 -0500

    Update build-offline.sh to save Slegehammer bits to a better location

--- tools/build-offline.sh
+++ tools/build-offline.sh
@@ -12,18 +12,19 @@ tmpdir="$(mktemp -d /tmp/rebar-offline-XXXXXX)" || exit 1
 echo "Exporting Digital Rebar containers"
 "./containers/export-containers" "$tmpdir/rebar-containers.tar.xz"
 echo "Copying the Digital Rebar source code"
-git clone --bare . "$tmpdir/digitalrebar.git"
+git clone --bare --no-hardlinks . "$tmpdir/digitalrebar.git"
+(cd "$tmpdir/digitalrebar.git"; git repack -a -d && git prune)
 echo "Fetching the current version of Sledgehammer"
-mkdir "$tmpdir/sledgehammer"
 (
     . deploy/compose/common.env
     cacheloc="$HOME/.cache/digitalrebar/tftpboot/sledgehammer/$PROV_SLEDGEHAMMER_SIG"
+    dest="sledgehammer/$PROV_SLEDGEHAMMER_SIG"
+    mkdir -p "$tmpdir/$dest" "$cacheloc"
     for f in sha1sums vmlinuz0 stage1.img stage2.img; do
-        if [[ -d $cacheloc && -f $cacheloc/$f ]]; then
-           cp "$cacheloc/$f" "$tmpdir/sledgehammer"
-        else
-            (cd "$tmpdir/sledgehammer" && curl -fgLO "$PROV_SLEDGEHAMMER_URL/$PROV_SLEDGEHAMMER_SIG/$f")
+        if ! [[ -d $cacheloc && -f $cacheloc/$f ]]; then
+            (cd "$cacheloc" && curl -fgLO "$PROV_SLEDGEHAMMER_URL/$PROV_SLEDGEHAMMER_SIG/$f")
         fi
+        cp "$cacheloc/$f" "$tmpdir/$dest/$f"
     done
 )
 (
